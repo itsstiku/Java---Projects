0afe3e295272a66785e28a1fe929b7bdc6ba30dd
Anubhab Ghosh <155558568+itsstiku@users.noreply.github.com>
2025-06-22 12:04:26 +0530

    ArrayMaxMin.java

--- ArrayLargest.java
+++ ArrayLargest.java
@@ -1,15 +1,16 @@
-//Find largest value from the arry
-package DsaJavaSeries;
+//Find out the Smallest & largest value from the arry.
 
+package DsaJavaSeries;
 public class Arrays {
     public static void main(String[] args) {
         int[] arr={10,20,13,26,4,7,8,6,3,16};
-        int smallNo=Integer.MIN_VALUE;
+        int smallNo=Integer.MAX_VALUE;
+        int largeNo=Integer.MIN_VALUE;
         for(int i=0;i<arr.length;i++){
-           if(arr[i]>smallNo){
-               smallNo=arr[i];
-           }
+           smallNo=Math.min(arr[i],smallNo);
+           largeNo=Math.max(arr[i],largeNo);
         }
-        System.out.println("Smallest Value is:"+smallNo);
+        System.out.println("Smallest Value is: "+smallNo);//3
+        System.out.println("Largest Value is: "+largeNo);//26
     }
 }
